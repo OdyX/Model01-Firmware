59c488a05a47568fb9d9476887f98e00dc49d320
Didier Raboud <odyx@debian.org>
2019-12-27 11:37:25 +0100

    Use AltGr modifiers instead

--- keymaps.h
+++ keymaps.h
@@ -58,8 +58,8 @@
 
 //#include "modifiers-std.h"
 //#include "modifiers-thumb-enter.h"
-#include "modifiers-thumb-enter-bepo.h"
-//#include "modifiers-altgr.h"
+//#include "modifiers-thumb-enter-bepo.h"
+#include "modifiers-altgr.h"
 //#include "modifiers-japan.h"
 
 /*
